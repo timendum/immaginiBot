9eff5da4e877e10f94cc4488c2bed7b3f0290674
Timendum <timedum@gmail.com>
2018-04-26 11:13:48 +0200

    Export: ordered list and generic method

--- export.py
+++ export.py
@@ -1,23 +1,28 @@
 """Export database"""
+from io import StringIO
+from collections import OrderedDict
+
 from sqlalchemy import distinct
 
-from database import Keyword, Image, db
+from database import Image, Keyword, db
 
 
-def export_md(add_hidden=False):
+def _export(add_hidden=False):
     """Export database to markdown"""
     imagesets = [key[0] for key in db.query(distinct(Image.imageset)).all()]
-    data = {}
+    imagesets = sorted(imagesets)
+    data = OrderedDict()
     for imageset in imagesets:
-        dbkeys = db.query(Keyword).filter_by(imageset=imageset)
+        dbkeys = db.query(Keyword).filter_by(imageset=imageset).filter(Keyword.keyword != imageset)
         if not add_hidden:
             dbkeys = dbkeys.filter_by(hidden=False)
-        keywords = [keyword.keyword for keyword in dbkeys.all()]
+        dbkeys = dbkeys.order_by(Keyword.keyword)
+        keywords = [imageset] + [keyword.keyword for keyword in dbkeys.all()]
         data[imageset] = {
             'urls': [image.image for image in db.query(Image).filter_by(imageset=imageset).all()],
             'keywords': keywords
         }
-    with open('export.md', mode='wt', encoding='utf8') as ofile:
+    with StringIO() as ofile:
         ofile.write('|Parola|Immagini|\n')
         ofile.write('|:-|:-|\n')
         for item in data.values():
@@ -27,7 +32,11 @@ def export_md(add_hidden=False):
             images = ['[%d](%s)' % ((idx + 1), url) for idx, url in enumerate(item['urls'])]
             ofile.write(' '.join(images))
             ofile.write('|\n')
+        return ofile.getvalue()
 
+def export_md(add_hidden=False):
+    with open('export.md', mode='wt', encoding='utf8') as ofile:
+        ofile.write(_export(add_hidden))
 
 if __name__ == "__main__":
     export_md()
