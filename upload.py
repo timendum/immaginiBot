ad237340ede8a9a479fbcda62a1ccb3fa7846ce9
timendum <timedum@gmail.com>
2022-05-20 17:35:25 +0200

    New Imgur upload

--- upload.py
+++ upload.py
@@ -2,36 +2,48 @@
 import configparser
 import sys
 
-from imgurpython import ImgurClient
-
 CONFIG = configparser.RawConfigParser()
-CONFIG.read('praw.ini')
+CONFIG.read("praw.ini")
 
 
-IMGUR_CLIENT_ID = CONFIG['IMGUR']['IMGUR_CLIENT_ID']
-IMGUR_CLIENT_SECRET = CONFIG['IMGUR']['IMGUR_CLIENT_SECRET']
-IMGUR_ALBUM_ID = CONFIG['IMGUR']['IMGUR_ALBUM_ID']
-# to obtain IMGUR_ACCESS_TOKEN and IMGUR_REFRESH_TOKEN
-# imgur = ImgurClient(IMGUR_CLIENT_ID, IMGUR_CLIENT_SECRET)
-# print(imgur.get_auth_url('pin'))
-# credentials = imgur.authorize('PIN_OBTAINED_FROM_URL_ABOVE', 'pin')
-# print(credentials)
-IMGUR_ACCESS_TOKEN = CONFIG['IMGUR']['IMGUR_ACCESS_TOKEN']
-IMGUR_REFRESH_TOKEN = CONFIG['IMGUR']['IMGUR_REFRESH_TOKEN']
+IMGUR_CLIENT_ID = CONFIG["IMGUR"]["IMGUR_CLIENT_ID"]
+IMGUR_CLIENT_SECRET = CONFIG["IMGUR"]["IMGUR_CLIENT_SECRET"]
+IMGUR_ALBUM_ID = CONFIG["IMGUR"]["IMGUR_ALBUM_ID"]
+# to obtain IMGUR_REFRESH_TOKEN
+# see https://apidocs.imgur.com/#authorization-and-oauth
+IMGUR_REFRESH_TOKEN = CONFIG["IMGUR"]["IMGUR_REFRESH_TOKEN"]
 
 
 def main():
-    imgur = ImgurClient(IMGUR_CLIENT_ID, IMGUR_CLIENT_SECRET, IMGUR_ACCESS_TOKEN,
-                        IMGUR_REFRESH_TOKEN)
+
+    response = requests.request(
+        "POST",
+        "https://api.imgur.com/oauth2/token",
+        data={
+            "refresh_token": IMGUR_REFRESH_TOKEN,
+            "client_id": IMGUR_CLIENT_ID,
+            "client_secret": IMGUR_CLIENT_SECRET,
+            "grant_type": "refresh_token",
+        },
+    )
+    response.raise_for_status()
+    access_token = response.json()["access_token"]
 
     url = sys.argv[1]
-    iconfig = {'album': IMGUR_ALBUM_ID, 'description': url}
-    img = imgur.upload_from_url(url, iconfig, False)
+    response = requests.request(
+        "POST",
+        "https://api.imgur.com/3/image",
+        headers={"Authorization": "Bearer " + access_token},
+        data={"image": url, "album": IMGUR_ALBUM_ID, "description": url},
+    )
+    response.raise_for_status()
+    newimg = response.json()
+
     try:
-        newurl = img['gifv']
+        newurl = newimg["data"]["gifv"]
     except KeyError:
-        newurl = img['link']
-    newurl = newurl.replace('http:', 'https:')
+        newurl = newimg["data"]["link"]
+    newurl = newurl.replace("http:", "https:")
     print('"%s" #%s' % (newurl, url))
 
 
